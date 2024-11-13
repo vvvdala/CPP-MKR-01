972052f9229d3db2c723f2c0c7d129bf77b10b7d
vvvdala <veronkievipad@gmail.com>
2024-11-13 09:38:05 +0200

    first commit

---
+++ CPP_MKR_01/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPP_MKR_01
+{
+    internal class Friend
+    {
+        public int Id { get; set; }
+
+        public int A { get; set; }
+
+        public int B { get; set; }
+    }
+}
